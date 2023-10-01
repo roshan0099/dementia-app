dadbd9aa07f81787e37825bfa2c1c5ca540037fb
roshan0099 <roshan09ka@gmail.com>
2023-10-01 15:49:03 +0530

    env

--- js/index_js.js
+++ js/index_js.js
@@ -1,6 +1,7 @@
 const keyVal = import.meta.env.VITE_CHECK
 
 console.log("01110010 01101111 01110011 01101000 01100001 01101110 : ",keyVal )
+console.log("hai")
 
 const btn = document.getElementById("button-contact");
 
