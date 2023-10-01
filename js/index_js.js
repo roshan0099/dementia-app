28b3fa090ca196479447000335e47c127859835f
roshan0099 <roshan09ka@gmail.com>
2023-10-01 19:12:02 +0530

    env

--- js/index_js.js
+++ js/index_js.js
@@ -1,7 +1,13 @@
+import { createClient } from '@supabase/supabase-js'
+
 const keyVal = import.meta.env.VITE_CHECK
 
+const sbURL = import.meta.env.VITE_SUPABASE_URL
+const sbKEY = import.meta.env.VITE_SUPABASE_KEY
+
+const submitBtn = document.getElementById("submit-form-btn")
+const form = document.forms["contact-form"]
 console.log("01110010 01101111 01110011 01101000 01100001 01101110 : ",keyVal )
-console.log("hai")
 
 const btn = document.getElementById("button-contact");
 
@@ -11,3 +17,38 @@ btn.addEventListener("click", () => {
 
 
 
+const supabase = createClient(sbURL, sbKEY)
+
+const { data, error } = await supabase
+  .from('customer_info')
+  .select("*")
+
+console.log("print => ",data)
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    console.log("you just clicked me", form, " == ", form["Name"].value)
+
+   async function formSubmission(){
+
+    try{
+        const { error } = await supabase
+        .from('customer_info')
+        .insert({
+        name: form["Name"].value,
+        phone : form["Number"].value,
+        email: form["Email"].value,
+        place : form["Place"].value,
+        message: form["Message"].value,
+
+        })
+      
+    }catch{
+        console.log("oops")
+    }
+
+    }
+        
+
+    formSubmission()
+})
